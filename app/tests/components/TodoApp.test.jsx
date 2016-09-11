e316486d35ce7e609f760508e52e38291aee4022
sfujimoto <unicoshun@gmail.com>
2016-09-11 22:33:56 +0900

    Add toggle support

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -20,4 +20,22 @@ describe('TodoApp', () =>{
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
   });
+
+  it('should toggle completed value when handleToggle called', () => {
+    let todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: false
+    }
+    let todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    // check the todos first item has completed value of false
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    // call handleToggle with 11
+    todoApp.handleToggle(11);
+    // Verify that value changed
+    expect(todoApp.state.todos[0].id).toBe(11);
+    expect(todoApp.state.todos[0].completed).toBe(true);
+  });
 });
