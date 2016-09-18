b4532e1d16e81f1814cff30102583405222a17a0
sfujimoto <unicoshun@gmail.com>
2016-09-18 20:07:02 +0900

    ~#118 add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -27,5 +27,41 @@ describe('Reducers', () => {
     });
   });
 
+  describe('todosReducer', () => {
+    it('shold add new todo', () => {
+      let action = {
+        type: 'ADD_TODO',
+        text: 'walk the dog'
+      };
+
+      let res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+
+    it('shold toggle todo', () => {
+      let todos = [
+        {
+          id: '123',
+          text: 'Something',
+          completed: true,
+          createdAt: 123,
+          completedAt: 125
+        }
+      ];
+      let action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+
+      let res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
+
 
 });
